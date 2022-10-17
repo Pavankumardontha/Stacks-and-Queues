88a6002338820b0212c80468a0fe1c74dc9b74f9
Pavankumardontha <pavankumardontha@gmail.com>
2022-10-17 21:04:27 +0530

    Update Stock_span.cpp

--- Stock_span.cpp
+++ Stock_span.cpp
@@ -4,25 +4,35 @@ class Solution
     //Function to calculate the span of stockâ€™s price for all n days.
     vector <int> calculateSpan(int price[], int n)
     {
-       stack<int> s;
        vector<int> span(n);
-       span[0]=1;
+       stack<int> s;
        s.push(0);
+       span[0]=1;
        for(int i=1;i<n;i++)
        {
+           /*
+           we have 2 choices , our current element can be 
+           1) greater than the topmost index element of the stack :- In this case we remove elements from stack till we get the element larger than current element.
+           Here by doing this we can encounter 2 things 
+           a) the stack might become empty : In this case we know that span is current_index + 1 
+           b) the stakc might not become empty and element present in the stack top index is greater than our current element : In this case the span is current_index - s.top() 
+           2) smaller than the topmost index element of the stack :- In this case , we just push the current element index into stack with span 1
+           */
            if(price[i]<price[s.top()])
-               span[i]=1;
+           span[i] = 1;
            else
-            {
-                while(!s.empty() and price[i]>=price[s.top()])
-                s.pop();
-                if(!s.empty())
-                span[i]=i-s.top();
-                else
-                span[i]=i+1;
-            }
+           {
+               while(!s.empty() and price[i]>=price[s.top()])
+               s.pop();
+               if(s.empty())
+               span[i]=i+1;
+               else
+               span[i]=i-s.top();
+           }
            s.push(i);
        }
        return span;
     }
 };
+
+
